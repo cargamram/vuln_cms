cc9d035631ee767116903bc010e2cd71b3bd2ef5
cargamram <carlosgamram@gmail.com>
2024-06-10 20:12:15 +0200

    modificado site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,31 +2,13 @@ node 'nodo01.domain.local' {
 
   drupal::site { 'vulncms.com':
     core_version => '8.2.0',
-    modules      => {
-      'ctools'   => '1.4',
-      'token'    => '1.5',
-      'pathauto' => '1.2',
-      'views'    => '3.8',
-    },
-    themes       => {
-      'omega' => '4.3',
-    },
-    libraries    => {
-      'jquery_ui' => {
-        'download' => {
-          'type' => 'file',
-          'url'  => 'http://jquery-ui.googlecode.com/files/jquery.ui-1.6.zip',
-          'md5'  => 'c177d38bc7af59d696b2efd7dda5c605',
-        },
-      },
-    },
   }
 
   class { 'apache':
     default_vhost => false,
   }
 
-  apache::vhost { 'drupal':
+  apache::vhost { 'vulncms.com':
     port    => 80,
     docroot => '/var/www/drupal',
     docroot_owner => 'www-data',
