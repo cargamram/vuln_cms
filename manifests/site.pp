367405daf3c8e9d948dcc5a6896646dfe8252f04
cargamram <carlosgamram@gmail.com>
2024-06-12 19:47:22 +0200

    instalación sin drupal

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,5 @@
 node 'nodo01.domain.local' {
 
-  drupal::site { 'vulncms.com':
-    core_version => '8.2.0',
-  }
-
   class { 'apache': 
    default_vhost => false, 
    mpm_module => 'prefork', 
@@ -13,15 +9,15 @@ node 'nodo01.domain.local' {
     php_version => '7.0', 
   }
 
+  class { 'mysql::server':
+    root_password => 'rootpassword',
+  }
+
   apache::vhost { 'vulncms.com':
     port    => 80,
     docroot => '/var/www/drupal',
     docroot_owner => 'www-data',
     docroot_group => 'www-data',
-  }
-
-  class { 'mysql::server':
-    root_password => 'rootpassword',
   }
 
   mysql::db { 'drupaldb':
