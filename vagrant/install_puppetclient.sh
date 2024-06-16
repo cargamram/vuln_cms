f344f319cf64a11617a544561dd7ba9d15841441
cargamram <carlosgamram@gmail.com>
2024-06-16 21:04:26 +0200

    añadida la libreria php-xml necesaria para instalar drupal

--- vagrant/install_puppetclient.sh
+++ vagrant/install_puppetclient.sh
@@ -5,7 +5,7 @@ echo "                                  INSTALANDO PUPPETCLIENT                 
 echo "**************************************************************************************************"
 
 sudo apt-get update
-sudo apt-get install -y wget lsb-release apt-transport-https
+sudo apt-get install -y wget lsb-release apt-transport-https php-xml
 
 echo "*********************************"
 echo "    DESCARGANDO PUPPET CLIENT    "
